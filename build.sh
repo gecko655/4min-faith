e09b74b93d3821eb901aa84ba4ae8054da718d72
gecko655 <aqwsedrft1234@yahoo.co.jp>
2015-11-07 18:41:49 +0900

    Implemented switch for test

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-docker build -t gecko655/4min-faith .
+SECRETENV_PATH="secretenv_release"
+if [ $# -gt 0 ] && [ $1 = test ]; then
+  SECRETENV_PATH="secretenv_test"
+fi
+cp $SECRETENV_PATH secretenv
+docker build $BUILD_ARG -t gecko655/4min-faith .
+rm secretenv
