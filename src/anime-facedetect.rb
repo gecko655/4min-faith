734644118b9e2a356975e3389d8ec29929433cf1
gecko655 <aqwsedrft1234@yahoo.co.jp>
2015-11-13 23:59:23 +0900

    Filter too small/big image

--- src/anime-facedetect.rb
+++ src/anime-facedetect.rb
@@ -19,6 +19,7 @@ def facedetect(image_path)
   face_cascade.detect_multi_scale( frame_gray, faces, 1.1, 2, )
   puts faces.size
   faces.each_with_index do |face, i|
+    next if face.height<50||face.height>200||face.width<50||face.width>200
     face_image = frame.block(face)
     cv::imwrite("/tmp/test_"+i.to_s+".jpg",face_image)
   end
